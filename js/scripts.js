7f5d0ff0f2913baea4d520424b12f5e417362078
poppydaniel97 <poppy.daniel@icloud.com>
2021-08-14 13:03:33 +0100

    changes 14/08/21

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ console.log("Hello! Welcome to My Portfolio Site");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 641) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
-}
+ 
